abd3f4dce5e9d4f426e148a4c1c03cf23f9042c9
bondroid1322 <bondroid3.22@icloud.com>
2024-03-05 00:35:52 +0900

    remove: tailwindcss関連コードの削除

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
