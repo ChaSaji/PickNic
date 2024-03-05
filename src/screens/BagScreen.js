37f662db8207a8245ca76c48f6ee51fed4e90041
bondroid1322 <bondroid3.22@icloud.com>
2024-03-05 17:44:24 +0900

    add: 各ページファイルの設定

---
+++ src/screens/BagScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const BagScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>バッグ</Text>
+    </View>
+  );
+};
+
+export default BagScreen;
