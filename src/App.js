b9d81698d64d2170514692bb5885dad27e74a069
bondroid1322 <bondroid3.22@icloud.com>
2024-03-06 16:19:04 +0900

    merge: home画面にDBハンドラーの確認ボタンを移動

--- src/App.js
+++ src/App.js
@@ -1,47 +1,30 @@
-import React from 'react';
-import { StatusBar } from "expo-status-bar";
-import { Button,Text, View } from "react-native";
-import {AddItem, DropTable, MakeTableItem,getRecode} from "./DataBaseHander"
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import CameraScreen from "./screens/CameraScreen";
+import CookingScreen from "./screens/CookingScreen";
+import BagScreen from "./screens/BagScreen";
+import BadgeScreen from "./screens/BadgeScreen";
+import HomeScreen from "./screens/HomeScreen";
 
-// テーブルを作成するクエリ
-const createTableQuery = `
-  CREATE TABLE IF NOT EXISTS items (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Camera" component={CameraScreen} />
+      <Tab.Screen name="Cooking" component={CookingScreen} />
+      <Tab.Screen name="Bag" component={BagScreen} />
+      <Tab.Screen name="Badge" component={BadgeScreen} />
+    </Tab.Navigator>
   );
-`;
+}
 
 export default function App() {
-    // 関数Bを定義
-    const functionA = () => {
-      console.log('ボタンAが押されました');
-      //DataBaseHander.MakeTable();
-      MakeTableItem();
-    };
-    const functionB = () => {
-      console.log('ボタンBが押されました');
-      //DataBaseHander.MakeTable();
-      AddItem("AddItemTester");
-      AddItem("日本語テスト");
-    };
-    const functionC = () => {
-      console.log('アイテム一覧を表示します');
-      //getRecode();
-      getRecode();
-    }
-    const functionD = () => {
-      console.log('テーブルを削除します');
-      DropTable();
-    }
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="MakeTableItem" onPress={functionA} />
-      <Button title="AddItem" onPress={functionB} />
-      <Button title="SeeItem" onPress={functionC} />
-      <Button title="DropTable" onPress={functionD} />
-      
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
---
+++ src/screens/CookingScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const CookingScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>つくる</Text>
+    </View>
+  );
+};
+
+export default CookingScreen;
