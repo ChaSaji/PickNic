e68181b4a5529e15e6fb9643fb52efa24ae8336a
bondroid1322 <bondroid3.22@icloud.com>
2024-03-07 17:01:07 +0900

    change: バッジを真ん中より、スクロールできるように変更

--- src/screens/BadgeScreen.js
+++ src/screens/BadgeScreen.js
@@ -1,37 +1,25 @@
 import React from "react";
-import {
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    ScrollView,
-    View,
-    Image
-} from 'react-native';
+import { StyleSheet, Text, ScrollView, View, Image } from "react-native";
 
 const BadgeScreen = () => {
   return (
-    <View style={[styles.container, {
-      // Try setting `flexDirection` to `"row"`.
-      flexDirection: "column"
-    }]}>
-      <View style={{ flex: 1, backgroundColor: "pink", justifyContent: "center", alignItems: "center" }}>
-        <Text>称号</Text>
+    <View style={styles.container}>
+      <View style={styles.titleView}>
+        <Text style={styles.title}>称号</Text>
       </View>
-      <SafeAreaView style={{flex: 9}}>
-        <ScrollView style={styles.scrollView}>
-          <View style={styles.badge}>
-            <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat1.png',}} style={styles.icon}/>
-            <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat1.png',}} style={styles.icon}/>
-            <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat1.png',}} style={styles.icon}/>
-            <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat1.png',}} style={styles.icon}/>
-            <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat1.png',}} style={styles.icon}/>
-            <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat1.png',}} style={styles.icon}/>
-            <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat1.png',}} style={styles.icon}/>
-            <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat1.png',}} style={styles.icon}/>
-            <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat1.png',}} style={styles.icon}/>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.badgeView}>
+          {[...Array(100)].map((_, index) => (
+            <Image
+              key={index}
+              source={{
+                uri: "https://reactnative.dev/docs/assets/p_cat1.png",
+              }}
+              style={styles.icon}
+            />
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -41,96 +29,32 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
+  titleView: {
+    backgroundColor: "pink",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
   scrollView: {
-    backgroundColor: 'white',
-    marginHorizontal: 0,
+    flex: 1,
   },
-  text: {
-    fontSize: 42,
-  },
-  badge: {
-    flexWrap: 'wrap',
+  badgeView: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    backgroundColor: "aliceblue",
+    justifyContent: "space-around",
     marginTop: 8,
-    backgroundColor: 'aliceblue',
-    maxHeight: 400,
-    flexDirection: 'row',
+    marginHorizontal: 8,
   },
   icon: {
     width: 80,
     height: 80,
-  }
+  },
 });
 
 export default BadgeScreen;
-
-/*
-import React from "react";
-import {
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    ScrollView,
-    View,
-    Image,
-} from 'react-native';
-
-const ImageDisplay = ({ imagePath }) => {
-  return (
-    <Image source={{uri: `file://../../asset/${imagePath}`}} style={styles.image} />
-    //<Image source={require(`../../assets/${imagePath}`)} style={styles.image} />
-  );
-};
-
-const BadgeScreen = () => {
-  const imagePaths = [
-    'p_cat1.png',
-    'favicon.png',
-    'favicon.png',
-    'favicon.png',
-    'favicon.png',
-  ];
-  return (
-    <View style={[styles.container, {
-      flexDirection: "column"
-    }]}>
-      <SafeAreaView style={{flex: 1}}>
-        <ScrollView style={styles.scrollView}>
-          <View style={styles.badge}>
-            {imagePaths.map((path, index) => (
-            <ImageDisplay key={index} imagePath={path}
-            />))}
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-  scrollView: {
-    backgroundColor: 'white',
-    marginHorizontal: 0,
-  },
-  text: {
-    fontSize: 42,
-  },
-  badge: {
-    flexWrap: 'wrap',
-    marginTop: 8,
-    backgroundColor: 'aliceblue',
-    maxHeight: 400,
-    flexDirection: 'row',
-  },
-  image: {
-    width: 80,
-    height: 80,
-    resizeMode: 'cover',
-  },
-});
-
-
-export default BadgeScreen;*/
