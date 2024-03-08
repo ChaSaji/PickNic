a20d3db7f573a3589b7e568b452110430260fed5
bondroid1322 <bondroid3.22@icloud.com>
2024-03-08 21:19:01 +0900

    refactor: locationの管理方法、エラーの管理方法などの変更

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, Button, StyleSheet, Dimensions } from "react-native";
-// import { useFocusEffect } from "@react-navigation/native";
-import {
-  AddItem,
-  DropTable,
-  MakeTableItem,
-  getRecode,
-} from "../DataBaseHander";
+import { View, StyleSheet, Dimensions, Text } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
-// import { LocationSubscription } from "expo-location/build/LocationSubscription";
-import { LocationSubscription } from "expo-location";
 
 const { width, height } = Dimensions.get("window");
 const ASPECT_RATIO = width / height;
@@ -19,60 +10,59 @@ const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 const HomeScreen = () => {
   const [myLocation, setMyLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
+  const [granted, setGranted] = useState(false);
 
   useEffect(() => {
     (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-        return;
-      } 
-        
-      let myLocation = await Location.watchPositionAsync(
-      {
-        accuracy: Location.Accuracy.Highest,
-        timeInterval: 1000, //android専用
-        distanceInterval: 1, //
-      },
-      (myLocation) => {
-        setMyLocation(myLocation)
-        setLatitude(myLocation.coords.latitude)
-        setLongitude(myLocation.coords.longitude)
-        console.log("latitude, longitude: " + myLocation.coords.latitude +","+myLocation.coords.longitude)
-      })
+      let locationSubscription;
 
-      return () => LocationSubscription.remove()
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status === "granted") {
+        setGranted(true);
+        locationSubscription = await Location.watchPositionAsync(
+          {
+            accuracy: Location.Accuracy.Highest,
+            timeInterval: 1000, //android専用
+            distanceInterval: 1, //
+          },
+          (location) => {
+            setMyLocation(location);
+            console.log(
+              `latitude: ${location.coords.latitude}, longitude: ${location.coords.longitude}`
+            );
+          }
+        );
+        return () => {
+          if (locationSubscription) {
+            locationSubscription.remove();
+          }
+        };
+      }
     })();
   }, []);
 
-
-  let text = "Waiting..";
-  if (errorMsg) {
-    text = errorMsg;
-    console.log(text);
-  }
-
   return (
     <View>
-      {latitude != null && longitude != null && (
+      {granted === false ? (
+        <Text>Permission to access location was denied</Text>
+      ) : myLocation === null ? (
+        <Text>Obtaining location information...</Text>
+      ) : (
         <MapView
           style={styles.map}
           initialRegion={{
-            latitude: latitude,
-            longitude: longitude,
+            latitude: myLocation.coords.latitude,
+            longitude: myLocation.coords.longitude,
             latitudeDelta: LATITUDE_DELTA,
             longitudeDelta: LONGITUDE_DELTA,
           }}
         >
           <Marker
             coordinate={{
-              latitude: latitude,
-              longitude: longitude,
+              latitude: myLocation.coords.latitude,
+              longitude: myLocation.coords.longitude,
             }}
-          ></Marker>
+          />
         </MapView>
       )}
     </View>
