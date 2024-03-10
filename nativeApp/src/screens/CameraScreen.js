eaa0f39cbacdc24ea3c59cbfd9fcda6c5a2c9657
bondroid1322 <bondroid3.22@icloud.com>
2024-03-10 23:48:13 +0900

    change: 撮った写真をデータベースに登録できるように変更

--- nativeApp/src/screens/CameraScreen.js
+++ nativeApp/src/screens/CameraScreen.js
@@ -3,17 +3,27 @@ import { Camera } from "expo-camera";
 import { useState } from "react";
 import { Text, View, TouchableOpacity, SafeAreaView } from "react-native";
 import { useCamera } from "../context/CameraContext";
+import { insert_item } from "../lib/dataBaseHelper";
+import { Photo, PhotoElement } from "../lib/databaseQueryText";
+import { useLocation } from "../context/LocationContext";
 
 const CameraScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
   const [camera, setCamera] = useState(null);
-
-  const { setIsCameraEnabled, setPicture } = useCamera();
+  const { setIsCameraEnabled, setPicture, setInsertedPhotId } = useCamera();
+  const { location } = useLocation();
 
   const takePicture = async () => {
     if (camera) {
       const image = await camera.takePictureAsync();
       setPicture(image);
+      const photo = new PhotoElement();
+      photo.name = "photoName";
+      photo.ratitude = location.coords.latitude;
+      photo.longitude = location.coords.longitude;
+      photo.visited = 1;
+      photo.pass2Photo = image.uri;
+      insert_item(Photo.tablename, photo).then((id) => setInsertedPhotId(id));
       setIsCameraEnabled(false);
       navigation.navigate("Picture");
     }
