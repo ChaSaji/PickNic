0a305869375887602ff62580153cadc6ba57c448
bondroid1322 <bondroid3.22@icloud.com>
2024-03-10 14:55:03 +0900

    change: 表示数が増えた時にスクロールできるように変更しました。

--- nativeApp/src/screens/CookingScreen.js
+++ nativeApp/src/screens/CookingScreen.js
@@ -1,54 +1,66 @@
 import React from "react";
-import { Text, View,StyleSheet,Image,Button } from "react-native";
-import { StatusBar } from 'expo-status-bar';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Button,
+  ScrollView,
+} from "react-native";
 
 const CookingScreen = () => {
   return (
     <View style={styles.container}>
-      {[...Array(3)].map((_, index) => (
-        <View style={styles.recipView}>
-          <View style={{width: '30%',height: '100%',justifyContent: 'center',    alignItems: 'center',}}>
-            <Image
-              source={require("../../assets/icons8-camera-64.png")}
-              style={{ width: 75, height: 75 }}
-            />
-          </View>
-          <View style={{width: '40%',height: '100%',justifyContent: 'center'}}>
-            {[...Array(4)].map((_, index) => (
-              <Text>
-                ・じゃがいも
-              </Text>
-            ))}
-          </View>
-          <View style = {{width: '20%',justifyContent: 'center'}}>
-              <Button 
-                title="Cooking!"
-                onPress={()=> alert("ボタンが押されました")}
+      <ScrollView>
+        {[...Array(10)].map((_, index) => (
+          <View key={index} style={styles.recipView}>
+            <View
+              style={{
+                width: "30%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                source={require("../../assets/icons8-camera-64.png")}
+                style={{ width: 75, height: 75 }}
               />
+            </View>
+            <View
+              style={{ width: "40%", height: "100%", justifyContent: "center" }}
+            >
+              {[...Array(4)].map((_, index) => (
+                <Text>・じゃがいも</Text>
+              ))}
+            </View>
+            <View style={{ width: "20%", justifyContent: "center" }}>
+              <Button
+                title="Cooking!"
+                onPress={() => alert(`${index}番のボタンが押されました`)}
+              />
+            </View>
           </View>
-        </View>
-      ))}
+        ))}
+      </ScrollView>
     </View>
-
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    backgroundColor: "#FFF",
+    justifyContent: "flex-start",
   },
   recipView: {
     height: 100,
-    flexDirection: 'row',
-    alignItems: 'center',
-    width: '100%',
-    backgroundColor: '#F8DAD1',
-    margin: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "#F8DAD1",
+    marginVertical: 5,
   },
 });
 
 export default CookingScreen;
-
