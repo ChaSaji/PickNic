b8694699d72020bfe50a4e10f4fd978104363dc9
bondroid1322 <bondroid3.22@icloud.com>
2024-03-12 01:19:49 +0900

    add: badgeのUIに実データを流し込んだ

--- nativeApp/src/screens/BadgeScreen.js
+++ nativeApp/src/screens/BadgeScreen.js
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, ScrollView, View } from "react-native";
 import ItemCard from "../components/ItemCard";
+import { fetchData, selectData } from "../lib/dataBaseHelper";
+import { Badge, Meal, RO } from "../lib/databaseQueryText";
 
 const BadgeScreen = ({ navigation }) => {
-  const handleItemClick = (name) => {
-    navigation.navigate("BadgeDetail", {
-      recipeName: `${name}番のバッジ`,
+  const [badges, setBadges] = useState([]);
+  useEffect(() => {
+    fetchData(Badge.tablename).then((data) => {
+      console.log(data);
+      setBadges(data);
     });
+  }, []);
+
+  const handleItemClick = (badge) => {
+    selectData(
+      Meal.tablename,
+      Meal.elementsKey.badgeId,
+      RO.Eqqual,
+      badge.id
+    ).then((meals) =>
+      navigation.navigate("BadgeDetail", {
+        badge: badge,
+        meals: meals,
+      })
+    );
   };
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        {[...Array(31)].map((_, index) => (
+        {badges.map((badge, index) => (
           <ItemCard
             key={index}
-            source={require("../../assets/icons8-camera-64.png")}
+            source={badge.pass2Photo}
             name={String(index)}
-            onPress={() => handleItemClick(String(index))}
+            onPress={() => handleItemClick(badge)}
             backgroundColor="#b8d4f4"
             isTextVisiable={false}
           />
