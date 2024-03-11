f269ebbcab439306406c3b9856a75837da5c6172
bondroid1322 <bondroid3.22@icloud.com>
2024-03-11 23:34:29 +0900

    add: 画像パスとソースのマッピングオブジェクトの作成

--- nativeApp/src/components/ItemCard.js
+++ nativeApp/src/components/ItemCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Image, Text, TouchableOpacity, StyleSheet } from "react-native";
+import images from "../lib/images";
 
 // propsで受け取る値はサンプル
 const ItemCard = ({
@@ -24,7 +25,7 @@ const ItemCard = ({
 
   return (
     <TouchableOpacity style={styles.item} onPress={() => onPress(name)}>
-      <Image source={source} style={{ width: 75, height: 75 }} />
+      <Image source={images[source]} style={{ width: 75, height: 75 }} />
       {isTextVisiable && <Text style={{ fontSize: 30 }}>{text}</Text>}
     </TouchableOpacity>
   );
