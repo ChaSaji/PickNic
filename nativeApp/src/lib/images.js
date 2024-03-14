b8e0d6bf05d8fea413c6e1c45effbff53ca97172
bondroid1322 <bondroid3.22@icloud.com>
2024-03-14 11:52:59 +0900

    add: un通常名の画像を用意

--- nativeApp/src/lib/images.js
+++ nativeApp/src/lib/images.js
@@ -1,17 +1,30 @@
 const images = {
   curry: require("../../assets/curry.png"),
+  uncarry: require("../../assets/uncarry.png"),
   guratan: require("../../assets/guratan.png"),
+  unguratan: require("../../assets/unguratan.png"),
   gyoza: require("../../assets/gyoza.png"),
+  ungyoza: require("../../assets/ungyoza.png"),
   korokke: require("../../assets/korokke.png"),
+  unkorokke: require("../../assets/unkorokke.png"),
   miso: require("../../assets/miso.png"),
+  unmiso: require("../../assets/unmiso.png"),
   oden: require("../../assets/oden.png"),
+  unoden: require("../../assets/unoden.png"),
   pasta: require("../../assets/pasta.png"),
+  unpasta: require("../../assets/unpasta.png"),
   piza: require("../../assets/piza.png"),
+  unpiza: require("../../assets/unpiza.png"),
   ponchi: require("../../assets/ponchi.png"),
+  unponchi: require("../../assets/unponchi.png"),
   ramen: require("../../assets/ramen.png"),
+  unramen: require("../../assets/unramen.png"),
   sake: require("../../assets/sake.png"),
+  unsake: require("../../assets/unsake.png"),
   tkg: require("../../assets/tkg.png"),
+  untkg: require("../../assets/untkg.png"),
   unagi: require("../../assets/unagi.png"),
+  ununagi: require("../../assets/ununagi.png"),
   kyushokub: require("../../assets/kyushokub.png"),
   morningb: require("../../assets/morningb.png"),
   italyb: require("../../assets/italyb.png"),
@@ -35,8 +48,7 @@ const images = {
 
 const getImageSource = ({ pass2Photo, locked = 0, cooked = 1 }) => {
   const imageName =
-    // locked == 1 ? "lock" : cooked == 1 ? pass2Photo : "un" + pass2Photo;
-    locked == 1 ? "lock" : cooked == 1 ? pass2Photo : "un";
+    locked == 1 ? "lock" : cooked == 1 ? pass2Photo : "un" + pass2Photo;
   return images[imageName];
 };
 
