d7b738daacb1b774cc561546b4327fc7dca55e61
bondroid1322 <bondroid3.22@icloud.com>
2024-03-14 14:06:02 +0900

    change: 背景色の変更

--- nativeApp/src/screens/LoadingAnimationScreen.js
+++ nativeApp/src/screens/LoadingAnimationScreen.js
@@ -7,9 +7,9 @@ const LoadingAnimationScreen = () => {
   const animation = useRef(null);
   const randomAnimationNum = getRandomNum(3);
   const loadingAnimationMap = {
-    0 : require("../../assets/material_animation_1.json"), // トラクター
-    1 : require("../../assets/material_animation_2.json"), // ぐるぐる
-    2 : require("../../assets/material_animation_3.json"), // ネコ
+    0: require("../../assets/material_animation_1.json"), // トラクター
+    1: require("../../assets/material_animation_2.json"), // ぐるぐる
+    2: require("../../assets/material_animation_3.json"), // ネコ
   };
 
   const loadingAnimation = loadingAnimationMap[randomAnimationNum];
@@ -35,7 +35,7 @@ const LoadingAnimationScreen = () => {
 const styles = StyleSheet.create({
   animationContainer: {
     // lottieでカラーセット色々あるから試してみた方がいいかも
-    backgroundColor: "F8DAD1",
+    backgroundColor: "#FFDC83",
     alignItems: "center",
     justifyContent: "center",
     flex: 1,
