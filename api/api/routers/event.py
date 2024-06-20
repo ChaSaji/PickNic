2577780d1abfff664d1297dd49b7bb528a06e280
Ilekaede <asakuramotiyo@gmail.com>
2024-06-20 17:23:08 +0900

    fix: datetime型に修正

--- api/api/routers/event.py
+++ api/api/routers/event.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter
 from typing import List
+from datetime import datetime
 import api.schemes.event as event_schema
 
 router = APIRouter()
 
 @router.get("/events", response_model=List[event_schema.EventBase])
 async def list_events():
-    return [event_schema.EventBase(id=1, event_name="イベント1", organizer="主催者1", start_date="2024-05-10", end_date="2025-03-22")]
+    return [event_schema.EventBase(id=1, event_name="イベント1", organizer="主催者1", start_date=datetime(2024, 5, 10), end_date=datetime(2025, 3, 22))]
 
 @router.post("/events/create")
 async def create_event():
@@ -14,7 +15,7 @@ async def create_event():
 
 @router.get("/events/{event_id}", response_model=event_schema.EventDetail)
 async def read_event():
-    return event_schema.EventDetail(id=1, event_name="イベント1", organizer="主催者", start_date="2024-05-10", end_date="2025-03-22", overview="概要の説明", badge_img="ieyasu", target_img="hamamatsu_castle_img", target_name="hamamatsu_castle_name", latitude=12.345, longitude=234.5556)
+    return event_schema.EventDetail(id=1, event_name="イベント1", organizer="主催者", start_date=datetime(2024, 5, 10), end_date=datetime(2025, 3, 22), overview="概要の説明", badge_img="ieyasu", target_img="hamamatsu_castle_img", target_name="hamamatsu_castle_name", latitude=12.345, longitude=234.5556)
 
 @router.put("/events/{event_id}/edit")
 async def update_event():
