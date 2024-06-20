4ec0f5efda9ed3142292edd45ba7860b0df95890
Ilekaede <asakuramotiyo@gmail.com>
2024-06-20 11:42:05 +0900

    fix: プルリクでの指摘を修正，optical型を削除，エンドポイント"hello/"の処理を削除

--- api/api/schemes/event.py
+++ api/api/schemes/event.py
@@ -1,19 +1,17 @@
 from pydantic import BaseModel, Field
-from typing import Tuple
+from datetime import date
 
-class Event(BaseModel):
-    id:int
-    name:str = Field(None, title="イベント名")
-    organizer:str = Field(None, title="主催者")
-    term:str = Field(None, title="期間") # TODO: 日付型に変換するか？
+class EventBase(BaseModel):
+    id: int = None  # DBで自動生成されるIDを入れるので, とりあえずNULLにしました
+    event_name: str = Field(..., title="イベント名")
+    organizer: str = Field(..., title="主催者")
+    start_date: date = Field(..., title="開始日")
+    end_date: date = Field(..., title="終了日")
 
-class EventDetail(BaseModel):
-    id:int
-    event_name:str = Field(None, title="イベント名")
-    organizer:str = Field(None, title="主催者")
-    term:str = Field(None, title="期間") # TODO: 日付型に変換するか？
-    overview:str = Field(None, title="概要")
-    badge_img:str = Field(None, title="バッジ画像(Base64)")
-    target_img:str = Field(None, title="撮影対象画像(Base64)")
-    target_name:str = Field(None, title="撮影対象名前")
-    position:Tuple[float, float] = Field(None, title="撮影場所") # TODO: 座標の型どう？
+class EventDetail(EventBase): # EventBaseを継承する
+    overview: str = Field(..., title="概要")
+    badge_img: str = Field(..., title="バッジ画像(Base64)")
+    target_img: str = Field(..., title="撮影対象画像(Base64)")
+    target_name: str = Field(..., title="撮影対象名前")
+    latitude: float = Field(..., title="緯度")
+    longitude: float = Field(..., title="経度")
