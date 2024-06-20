f128daf1c9a54112575c44d8f1105525a1df4871
Ilekaede <asakuramotiyo@gmail.com>
2024-06-20 10:26:40 +0900

    add:POST，DELETEの処理を追加

--- api/api/routers/event.py
+++ api/api/routers/event.py
@@ -11,18 +11,32 @@ async def list_events():
 
 @router.post("/events/create", response_model=event_schema.EventCreateResponse)
 async def create_event(event_body: event_schema.EventCreate):
-    return event_schema.EventCreateResponse(**event_body.model_dump()) # 現状はidをNULLで返す
+    # コメントアウト部分は次回のプルリクで出します．
+    # new_id = 1 # ここはDBから取得する
+    # event_data = event_body.model_dump(exclude={'id'})
+    # return event_schema.EventCreateResponse(id=new_id, **event_data)
+    return event_schema.EventCreateResponse(**event_body.model_dump())
 
 @router.get("/events/{event_id}", response_model=List[event_schema.EventDetail])
 async def read_event():
+    #
+    # ここにreturn文に必要なデータをDBから取得する処理, 下には仮のものを入れてある
+    #
     return [event_schema.EventDetail(id=1, event_name="イベント1", organizer="主催者", term=date(2024, 5, 10), overview="概要の説明", badge_img="ieyasu", target_img="hamamatsu_castle_img", target_name="hamamatsu_castle_name", position=[12.345, 3456.777])]
 
-@router.put("/events/{event_id}/edit")
-async def update_event():
-    pass
+# NOTE:イベント情報を更新する処理はeventCreateと同様の処理でいいのか？という疑問あり
+@router.put("/events/{event_id}/edit", response_model=event_schema.EventCreateResponse)
+async def update_event(event_id: int, event_body: event_schema.EventCreateResponse):
+    event_data = event_body.model_dump()
+    event_data["id"] = event_id
 
-@router.delete("/events/{event_id}/delete")
-async def delete_event():
-    pass
+    return event_schema.EventCreateResponse(**event_data)
+
+@router.delete("/events/{event_id}/delete", response_model=None)
+async def delete_event(event_id: int):
+    #
+    # DB内のイベント情報を削除する処理
+    #
+    return # NULLを返す
 
 
