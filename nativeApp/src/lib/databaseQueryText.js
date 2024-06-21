243e6561fd781e0d011bb8cb10654b341106347e
bondroid1322 <bondroid3.22@icloud.com>
2024-06-21 21:20:25 +0900

    chore: databaseタブを閉じたよとエラーログのレベルを0にしたよ

--- nativeApp/src/lib/databaseQueryText.js
+++ nativeApp/src/lib/databaseQueryText.js
@@ -1,5 +1,5 @@
 //SQL Funcs
-export const debugDataBaseLevel = 2; //0:No Debug,1:Easy Debug,2:Full Debug;
+export const debugDataBaseLevel = 0; //0:No Debug,1:Easy Debug,2:Full Debug;
 
 export const PassToExcelTemplate = "../../DBtemplate/";
 export const FileExtension = ".xlsx";
