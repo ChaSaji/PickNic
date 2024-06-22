4f8d013a10004db76cf267a9d05d31085d8d72da
Karin <nockykarin@gmail.com>
2024-06-23 02:50:31 +0900

    fix インポート

--- api/api/main.py
+++ api/api/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from api.routers import picture2item
 
 app = FastAPI()
 
 @app.get("/hello")
 async def root():
     return {"message": "Hello World"}
+
+app.include_router(picture2item.router)
