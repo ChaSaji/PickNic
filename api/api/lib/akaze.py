3e296ad5a68ac036479677e4497531a66daeebc0
Karin <nockykarin@gmail.com>
2024-06-23 21:58:36 +0900

    特徴量マッチングのプログラムとイベントの画像処理に関するfastAPI

---
+++ api/api/lib/akaze.py
@@ -0,0 +1,42 @@
+import sys
+import cv2
+import numpy as np
+
+#pip install opencv-contrib-python
+
+def akaze(contents,original):
+    img1 = cv2.imread(contents)
+    img2 = cv2.imread(original)
+
+    gray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+
+    # AKAZE検出器の生成
+    akaze = cv2.AKAZE_create() 
+    # gray1にAKAZEを適用、特徴点を検出
+    kp1, des1 = akaze.detectAndCompute(gray1,None) 
+    # gray2にAKAZEを適用、特徴点を検出
+    kp2, des2 = akaze.detectAndCompute(gray2,None) 
+
+    # BFMatcherオブジェクトの生成
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    # Match descriptorsを生成
+    matches = bf.match(des1, des2)
+    dist = [m.distance for m in matches]
+    ret = sum(dist) / len(dist)
+    #0が同じ、100が違うなので逆にする
+    match_per=100.0-ret
+    round(match_per,5)
+    #print(match_per)
+
+    # matchesをdescriptorsの似ている順にソートする 
+    #matches = sorted(matches, key = lambda x:x.distance)
+
+    # 検出結果を描画する
+    #img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:10], None, flags = cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    #検出結果を描画した画像を出力する
+    #cv2.imwrite("result.jpg",img3)
+ 
+    return match_per
