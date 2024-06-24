48b501d3b2056ca44f811c768ffd58ae4a447caa
Ilekaede <asakuramotiyo@gmail.com>
2024-06-24 14:46:54 +0900

    idを後付けに修正

--- api/api/schemes/event.py
+++ api/api/schemes/event.py
@@ -2,13 +2,12 @@ from pydantic import BaseModel, Field
 from datetime import datetime
 
 class EventBase(BaseModel):
-    id: int = None  # DBで自動生成されるIDを入れるので, とりあえずNULLにしました
     event_name: str = Field(..., title="イベント名")
     organizer: str = Field(..., title="主催者")
     start_date: datetime = Field(..., title="開始日")
     end_date: datetime = Field(..., title="終了日")
 
-class EventDetail(EventBase): # EventBaseを継承する
+class EventDetailBase(EventBase): # EventBaseを継承する
     overview: str = Field(..., title="概要")
     badge_img: str = Field(..., title="バッジ画像(Base64)")
     target_img: str = Field(..., title="撮影対象画像(Base64)")
@@ -16,7 +15,12 @@ class EventDetail(EventBase): # EventBaseを継承する
     latitude: float = Field(..., title="緯度")
     longitude: float = Field(..., title="経度")
 
-class EventCreate(EventDetail):
+class Event(EventBase): # Event
+    id: int = None
+
+class EventDetail(EventDetailBase): # Event
+    id: int = None
+class EventCreate(EventDetailBase):
     pass
 
 class EventCreateResponse(EventCreate):
