58e3108c56d712b7a4c1e6d68bfbca1131f3c585
Karin <nockykarin@gmail.com>
2024-07-01 20:21:53 +0900

    テスト用コード

--- api/api/routers/events/match.py
+++ api/api/routers/events/match.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter, UploadFile, File
 from ...lib.akaze import akaze
+from fastapi.responses import JSONResponse
 
 router=APIRouter()
 
 #テンプレートどうやってもってくる？ランキングは？
 @router.post("/events/{event_id}/uploadfile/")
-async def upload_files(file1,file2):
-#async def upload_files(file1: UploadFile = File(...),file2: UploadFile = File(...)):
+#async def upload_files(file1,file2):
+async def upload_files(file1: UploadFile = File(...),file2: UploadFile = File(...)):
     contents = await file1.read()
     original = await file2.read()
     ret = akaze(contents,original)
--- api/api/routers/picture2item.py
+++ api/api/routers/picture2item.py
@@ -4,8 +4,8 @@ from ..lib.convert_colors import convert
 router=APIRouter()
 
 @router.post("/uploadfile/")
-async def upload_files(file):
-#async def upload_files(file: UploadFile = File(...)):
+#async def upload_files(file):
+async def upload_files(file: UploadFile = File(...)):
     contents = await file.read()
     ret = convert(contents)
     return {"return":str(ret)}
