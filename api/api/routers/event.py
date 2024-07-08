c281c9ed61b78fc964f4e9226f21264dbd225f69
Ilekaede <asakuramotiyo@gmail.com>
2024-07-08 20:08:34 +0900

    CRUD処理の作成

--- api/api/routers/event.py
+++ api/api/routers/event.py
@@ -1,42 +1,72 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from typing import List
 import api.schemes.event as event_schema
-from datetime import date
+from api.database import get_db
+import api.cruds.event as event_cruds
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
-@router.get("/events", response_model=List[event_schema.EventBase])
-async def list_events():
-    return [event_schema.EventBase(id=1, event_name="イベント1", organizer="主催者1", term=date(2024, 5, 10))]
+@router.get("/events", response_model=List[event_schema.Event])
+def list_events(db:Session=Depends(get_db)):
+    return event_cruds.get_event_list(db)
 
-@router.post("/events/create", response_model=event_schema.EventCreateResponse)
-async def create_event(event_body: event_schema.EventCreate):
-    # コメントアウト部分は次回のプルリクで出します．
-    # new_id = 1 # ここはDBから取得する
-    # event_data = event_body.model_dump(exclude={'id'})
-    # return event_schema.EventCreateResponse(id=new_id, **event_data)
-    return event_schema.EventCreateResponse(**event_body.model_dump())
+@router.post("/events/create", response_model=event_schema.EventCreateResponse) #TODO: 同じ名前のイベントでも複数登録できてしまうので変える必要あります．
+def create_event(event_body: event_schema.EventCreate, db:Session = Depends(get_db)):
+    db_organization_id = event_cruds.create_organization(db, event_body.organization)
 
-@router.get("/events/{event_id}", response_model=List[event_schema.EventDetail])
-async def read_event():
-    #
-    # ここにreturn文に必要なデータをDBから取得する処理, 下には仮のものを入れてある
-    #
-    return [event_schema.EventDetail(id=1, event_name="イベント1", organizer="主催者", term=date(2024, 5, 10), overview="概要の説明", badge_img="ieyasu", target_img="hamamatsu_castle_img", target_name="hamamatsu_castle_name", position=[12.345, 3456.777])]
+    db_event_id = event_cruds.create_event(db, event_body, db_organization_id)
 
-# NOTE:イベント情報を更新する処理はeventCreateと同様の処理でいいのか？という疑問あり
-@router.put("/events/{event_id}/edit", response_model=event_schema.EventCreateResponse)
-async def update_event(event_id: int, event_body: event_schema.EventCreateResponse):
-    event_data = event_body.model_dump()
-    event_data["id"] = event_id
+    db_photo_id = event_cruds.create_photo(db, event_body, db_event_id)
 
-    return event_schema.EventCreateResponse(**event_data)
+    db_event_badge = event_cruds.create_event_badge(db, event_body, db_event_id)
+
+    create_event_data = event_body.model_dump()
+    create_event_data["event_id"] = db_event_id
+    create_event_data["organization_id"] = db_organization_id
+    create_event_data["badge_id"] = db_event_badge
+    create_event_data["photo_id"] = db_photo_id
+    return event_schema.EventCreateResponse(**create_event_data)
+
+@router.get("/events/{event_id}", response_model=event_schema.EventDetail)
+def read_event(event_id: int, db: Session = Depends(get_db)):
+    return event_cruds.get_event_detail(db, event_id)
+
+# NOTE:organization_idをリクエストで受けとる必要がある．
+@router.put("/events/{event_id}/edit", response_model=event_schema.EventUpdateResponse)
+def update_event(event_id: int, organization_id: int, photo_id: int, badge_id:int, event_body: event_schema.EventUpdate, db: Session=Depends(get_db)):
+
+    event_cruds.update_organization(db, organization_id, event_body.organization)
+    event_cruds.update_event(db, event_id, event_body)
+    event_cruds.update_photo(db, photo_id, event_body)
+    event_cruds.update_badge(db, badge_id, event_body)
+
+    update_info = event_schema.EventUpdateResponse(
+            event_id = event_id,
+            organization_id = organization_id,
+            badge_id= badge_id,
+            photo_id = photo_id,
+            event_name=event_body.event_name,
+            organization=event_body.organization,
+            start_date=event_body.start_date,
+            end_date=event_body.end_date,
+            overview=event_body.overview,
+            # TODO: 以下がマジックナンバーなので直す必要あります
+            badge_img=event_body.badge_img,
+            badge_name=event_body.badge_name,
+            target_img=event_body.target_img,
+            target_name=event_body.target_name,
+            latitude= event_body.latitude,
+            longitude=event_body.longitude,
+            status = event_body.status
+
+        )
+
+    return update_info
 
 @router.delete("/events/{event_id}/delete", response_model=None)
-async def delete_event(event_id: int):
-    #
-    # DB内のイベント情報を削除する処理
-    #
-    return # NULLを返す
+def delete_event(event_id: int, db: Session=Depends(get_db)):
+    return event_cruds.delete_event(db, event_id)
+
 
 
--- api/api/schemes/event.py
+++ api/api/schemes/event.py
@@ -1,26 +1,58 @@
 from pydantic import BaseModel, Field
-from typing import Tuple, Optional
-from datetime import date
+from datetime import datetime
 
 class EventBase(BaseModel):
-    id: Optional[int] = None  # DBで自動生成されるIDを入れるので, とりあえずNULLにしました
-    event_name: Optional[str] = Field(None, title="イベント名")
-    organizer: Optional[str] = Field(None, title="主催者")
-    term: Optional[date] = Field(None, title="期間")  # 日付型
+    event_name: str = Field(..., title="イベント名")
+    organization: str = Field(..., title="主催団体")
+    start_date: datetime = Field(..., title="開始日")
+    end_date: datetime = Field(..., title="終了日")
+
+class EventDetailBase(EventBase): # EventBaseを継承する
+    overview: str = Field(..., title="概要")
+    badge_img: str = Field(..., title="バッジ画像(Base64)")
+    badge_name: str = Field(..., title="バッジ名")
+    target_img: str = Field(..., title="撮影対象画像(Base64)")
+    target_name: str = Field(..., title="撮影対象名前")
+    latitude: float = Field(..., title="緯度")
+    longitude: float = Field(..., title="経度")
+
+class Event(EventBase): # Event
+    event_id: int = Field(..., title="イベント番号")
+    # organization_id: int = Field(..., title="団体番号")
+    # badge_id: int = Field(..., title="バッジ番号")
+    # photo_id: int = Field(..., title="写真番号")
 
     class Config:
         from_attributes = True
 
-class EventDetail(EventBase): # EventBaseを継承する
-    overview: Optional[str] = Field(None, title="概要")
-    badge_img: Optional[str] = Field(None, title="バッジ画像(Base64)")
-    target_img: Optional[str] = Field(None, title="撮影対象画像(Base64)")
-    target_name: Optional[str] = Field(None, title="撮影対象名前")
-    position: Optional[Tuple[float, float]] = Field(None, title="撮影場所")  # 座標型
-
-class EventCreate(EventDetail):  # EventDetailを継承する
+class EventDetail(EventDetailBase):
+    event_id: int = Field(..., title="イベント番号")
     pass
+class EventCreate(EventDetailBase):
+    pass
+class EventUpdate(EventDetailBase):
+    status: bool = Field(..., title="情報公開の有無")
 
-class EventCreateResponse(EventDetail):
+class EventCreateResponse(EventCreate):
+    event_id: int = Field(..., title="ID")
+    organization_id: int = Field(..., title="団体番号")
+    badge_id: int = Field(..., title="バッジ番号")
+    photo_id: int = Field(..., title="写真番号")
+    # class Config:
+    #     from_attributes = True
+
+class EventUpdateResponse(EventUpdate):
+    event_id: int = Field(..., title="ID")
+    organization_id: int = Field(..., title="団体番号")
+    badge_id: int = Field(..., title="バッジ番号")
+    photo_id: int = Field(..., title="写真番号")
+    class Config:
+        from_attributes = True
+
+class EventDeleteResponse(EventDetail):
+    event_id: int = Field(..., title="ID")
+    organization_id: int = Field(..., title="団体番号")
+    badge_id: int = Field(..., title="バッジ番号")
+    photo_id: int = Field(..., title="写真番号")
     class Config:
         from_attributes = True
