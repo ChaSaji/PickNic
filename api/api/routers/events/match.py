90ac7202ca24eb5ab4872a32f59d2d01a53d8fcc
Karin <nockykarin@gmail.com>
2024-07-08 16:14:03 +0900

    ディレクトリから画像ファイルを持ってこれるようにしたよ

--- api/api/routers/events/match.py
+++ api/api/routers/events/match.py
@@ -1,13 +1,28 @@
 from fastapi import APIRouter, UploadFile, File
 from ...lib.akaze import akaze
 from fastapi.responses import JSONResponse
+from pathlib import Path
+import aiofiles
 
 router=APIRouter()
 
 #テンプレートどうやってもってくる？ランキングは？
 @router.post("/events/{event_id}/uploadfile/")
-#async def upload_files(file1,file2):
-async def upload_files(file1: UploadFile = File(...),file2: UploadFile = File(...)):
+async def upload_files(file1: UploadFile = File(...)):
+    contents = await file1.read()
+    # Path to the static file2 in the directory
+    file2_path = Path("./banana.jpg")
+    
+    # Read the content of file2 from the directory
+    async with aiofiles.open(file2_path, 'rb') as file2:
+        original = await file2.read()
+
+    ret = akaze(contents,original)
+    return {"return":str(ret)}
+
+
+@router.post("/events/{event_id}/uploadfile/sub")
+async def upload_files_sub(file1: UploadFile = File(...),file2: UploadFile = File(...)):
     contents = await file1.read()
     original = await file2.read()
     ret = akaze(contents,original)
--- api/api/routers/picture2item.py
+++ api/api/routers/picture2item.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, UploadFile, File
-from ..lib.convert_colors import convert
+from api.lib.convert_colors import convert
 
 router=APIRouter()
 
 @router.post("/uploadfile/")
-#async def upload_files(file):
 async def upload_files(file: UploadFile = File(...)):
     contents = await file.read()
     ret = convert(contents)
