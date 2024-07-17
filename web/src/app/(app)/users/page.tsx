4133bb43561e450519814581767f8aa2ff2ffe35
bondroid1322 <bondroid3.22@icloud.com>
2024-07-18 01:46:22 +0900

    add: user一覧ページを実装したよ

---
+++ web/src/app/(app)/users/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+import React from "react";
+import { useTable } from "react-table";
+import PageTemplate from "@/components/PageTemplate/PageTemplate";
+import Button from "@/components/Button/Button";
+import { useRouter } from "next/navigation";
+import { Column } from "react-table";
+import { useState, useEffect } from "react";
+import { getUserList } from "@/lib/api/user";
+import { User } from "@/types/user";
+
+const columns: Column<User>[] = [
+  { Header: "ユーザ名", accessor: "name" },
+  { Header: "メールアドレス", accessor: "email" },
+];
+
+export default function UserListPage() {
+  const [users, setUsers] = useState<Array<User>>([]);
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data: users });
+  const router = useRouter();
+  const handleNavigate = () => {
+    router.push(`/users/create`);
+  };
+
+  useEffect(() => {
+    (async () => {
+      const users = await getUserList();
+      users.data && setUsers(users.data);
+    })();
+  }, []);
+
+  return (
+    <PageTemplate titleLabel="ユーザ一覧">
+      <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "end",
+            padding: 0,
+            marginBottom: 10,
+          }}
+        >
+          <Button onClick={handleNavigate} label="ユーザ登録" />
+        </div>
+
+        <table
+          style={{
+            textAlign: "center",
+            width: "100%",
+            borderSpacing: 0,
+            tableLayout: "fixed",
+            border: "solid 1px #000",
+          }}
+          {...getTableProps()}
+        >
+          <thead
+            style={{
+              textAlign: "center",
+            }}
+          >
+            {headerGroups.map((headerGroup, idx) => (
+              <tr {...headerGroup.getHeaderGroupProps()} key={idx}>
+                {headerGroup.headers.map((column, idx) => (
+                  <th
+                    style={{
+                      width: "80%",
+                      tableLayout: "fixed",
+                      border: "1.5px #7e7e7e solid",
+                    }}
+                    {...column.getHeaderProps()}
+                    key={idx}
+                  >
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()} key={i}>
+                  {row.cells.map((cell, i) => {
+                    return (
+                      <td
+                        style={{
+                          width: "80%",
+                          tableLayout: "fixed",
+                          border: "1.5px #7e7e7e solid",
+                        }}
+                        {...cell.getCellProps()}
+                        key={i}
+                      >
+                        {cell.render("Cell")}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </PageTemplate>
+  );
+}
--- web/src/lib/api/user.ts
+++ web/src/lib/api/user.ts
@@ -1,8 +1,32 @@
 import { userSchemaType } from "@/schemas/userSchema";
 import { ApiResponse } from "@/types/utils";
 import { fetchAPIWithAuth } from "./helper";
-import AuthUser from "@/types/user";
+import AuthUser, { User } from "@/types/user";
 import { ApiError } from "./ApiError";
+
+export const getUserList = async (): Promise<ApiResponse<Array<User>>> => {
+  try {
+    const response = await fetchAPIWithAuth({
+      endpoint: "auth/users/",
+      method: "GET",
+    });
+
+    // TODO: userがany型になってしまった。fetchAPIにジェネリックで型指定する必要有
+    const data = response.users.map((user: any) => ({
+      id: user.id,
+      name: user.username,
+      email: user.email,
+    }));
+
+    return { success: true, message: "ユーザ取得に成功しました。", data };
+  } catch (error) {
+    let userMessage = "エラーが発生しました。";
+    if (error instanceof ApiError && error.status === 400) {
+      userMessage = error.detail;
+    }
+    return { success: false, message: userMessage };
+  }
+};
 
 export const postUserForm = async ({
   username,
