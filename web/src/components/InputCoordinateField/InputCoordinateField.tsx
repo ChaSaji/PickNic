9af05bf26f5adbddcb8e457345e4b4c233b5153d
bondroid1322 <bondroid3.22@icloud.com>
2024-07-18 18:30:09 +0900

    add: defaultValueを設定できるようにしたよ

--- web/src/components/InputCoordinateField/InputCoordinateField.tsx
+++ web/src/components/InputCoordinateField/InputCoordinateField.tsx
@@ -1,39 +1,43 @@
-import { eventSchemaType } from "@/schemas/eventSchema"
+import { eventSchemaType } from "@/schemas/eventSchema";
 import InputField from "@/components/InputField/InputField";
 
 type PropsType = {
-    latitudeTag: keyof eventSchemaType
-    longitudeTag: keyof eventSchemaType
-}
+  latitudeTag: keyof eventSchemaType;
+  longitudeTag: keyof eventSchemaType;
+  defaultValue?: {
+    latitude: number;
+    longitude: number;
+  };
+};
 
-const InputCoordinateField = (props:PropsType) => {
-    return (
-        <div>
-            <div>
-              撮影物の座標
-            </div>
-            <div style={{display: "flex", gap: 10}}>
-              <InputField
-                type="number"
-                step={0.00001}
-                size="small"
-                direction="row"
-                name={props.latitudeTag}
-                label={"緯度"}
-                placeholder={""}
-              />
-              <InputField
-                type="number"
-                step={0.00001}
-                size="small"
-                direction="row"
-                name={props.longitudeTag}
-                label={"経度"}
-                placeholder={""}
-              />
-            </div>
-          </div>
-    )
-}
+const InputCoordinateField = (props: PropsType) => {
+  return (
+    <div>
+      <div>撮影物の座標</div>
+      <div style={{ display: "flex", gap: 10 }}>
+        <InputField
+          type="number"
+          step={0.00001}
+          size="small"
+          direction="row"
+          name={props.latitudeTag}
+          label={"緯度"}
+          placeholder={""}
+          defaultValue={props.defaultValue && props.defaultValue.latitude}
+        />
+        <InputField
+          type="number"
+          step={0.00001}
+          size="small"
+          direction="row"
+          name={props.longitudeTag}
+          label={"経度"}
+          placeholder={""}
+          defaultValue={props.defaultValue && props.defaultValue.longitude}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default InputCoordinateField
+export default InputCoordinateField;
--- web/src/components/InputFileField/InputFileField.tsx
+++ web/src/components/InputFileField/InputFileField.tsx
@@ -4,51 +4,56 @@ import { useFormContext } from "react-hook-form";
 import InputField from "@/components/InputField/InputField";
 
 type PropsType = {
-    title: string
-    imageTag: keyof eventSchemaType
-    nameTag: keyof eventSchemaType
-}
+  title: string;
+  imageTag: keyof eventSchemaType;
+  nameTag: keyof eventSchemaType;
+  defaultValue?: {
+    image: string;
+    name: string;
+  };
+};
 
-const InputFileField = (props:PropsType) => {
-    const [imageSrc, setImageSrc] = useState<string | null>(null);
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      if (event.target.files && event.target.files.length > 0) {
-        const file = event.target.files[0];
-        setImageSrc(window.URL.createObjectURL(file))
-      }
+const InputFileField = (props: PropsType) => {
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      setImageSrc(window.URL.createObjectURL(file));
     }
-    const {
-        register,
-        formState: { errors },
-      } = useFormContext();
+  };
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
   return (
     <div>
       <div>
         <InputField
-            size="medium"
-            direction="column"
-            name={props.nameTag}
-            label={props.title+"の名前"}
-            placeholder={""}
-          />
-        </div>
-        {props.title}
-        <br/>
-        <input
-          type="file"
-          accept="image/*"
-          {...register(props.imageTag)}
-          onChange={handleFileChange}
+          size="medium"
+          direction="column"
+          name={props.nameTag}
+          label={props.title + "の名前"}
+          placeholder={""}
+          defaultValue={props.defaultValue && props.defaultValue.name}
         />
-        {errors[props.imageTag] && (
-            <div style={{ color: "red" }}>
-                {`${errors[props.imageTag]?.message}`}
-            </div>
-        )}
-        <br/>
-       {imageSrc && <img src={imageSrc} alt="" style={{ width: 300, }}/>}
       </div>
-  )
-}
+      {props.title}
+      <br />
+      <input
+        type="file"
+        accept="image/*"
+        {...register(props.imageTag)}
+        onChange={handleFileChange}
+      />
+      {errors[props.imageTag] && (
+        <div style={{ color: "red" }}>
+          {`${errors[props.imageTag]?.message}`}
+        </div>
+      )}
+      <br />
+      {imageSrc && <img src={imageSrc} alt="" style={{ width: 300 }} />}
+    </div>
+  );
+};
 
-export default InputFileField
+export default InputFileField;
