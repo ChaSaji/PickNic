7173217d3feea0b2444d042d0968340cccd79693
bondroid1322 <bondroid3.22@icloud.com>
2024-07-18 17:12:26 +0900

    add: APIにイベント登録できるようにしたよ

--- web/src/app/(app)/events/create/page.tsx
+++ web/src/app/(app)/events/create/page.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
-import PageTemplate from "@/components/PageTemplate/PageTemplate"
-import { useForm, SubmitHandler, FormProvider } from "react-hook-form"
+import PageTemplate from "@/components/PageTemplate/PageTemplate";
+import { useForm, FormProvider } from "react-hook-form";
 import InputField from "@/components/InputField/InputField";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { eventSchemaType, eventSchema } from "@/schemas/eventSchema";
@@ -9,55 +9,70 @@ import InputFileField from "@/components/InputFileField/InputFileField";
 import InputDateField from "@/components/InputDateField/InputDateField";
 import InputCoordinateField from "@/components/InputCoordinateField/InputCoordinateField";
 import InputDescriptionField from "@/components/InputDescriptionField/InputDescriptionField";
-
+import { useApiSubmit } from "@/hooks/useApiSubmit";
+import { Event } from "@/types/event";
+import { postEventForm } from "@/lib/api/event";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function App() {
-
+  const { onApiSubmit, apiResponse } = useApiSubmit<eventSchemaType, Event>(
+    postEventForm
+  );
   const formMethods = useForm<eventSchemaType>({
     resolver: zodResolver(eventSchema),
-  })
+  });
 
-  const onSubmit: SubmitHandler<eventSchemaType> = (data) => {
-    console.log(data)
-  }
+  const router = useRouter();
+
+  useEffect(() => {
+    if (apiResponse?.success) {
+      formMethods.reset();
+      router.push("/events");
+    }
+  }, [apiResponse]);
 
   return (
     <PageTemplate titleLabel="イベント登録">
       <FormProvider {...formMethods}>
-        <form onSubmit={formMethods.handleSubmit(onSubmit)}>
+        <form onSubmit={formMethods.handleSubmit(onApiSubmit)}>
           <InputField
             size="medium"
             direction="column"
-            name={"organizer"}
-            label={"主催者名"}
-            placeholder={"ぴくにく太郎"}
-          />
-
-          <InputField
-            size="medium"
-            direction="column"
-            name={"eventName"}
+            name={"name"}
             label={"イベント名"}
             placeholder={"ぴくにく祭"}
           />
 
-          <InputDescriptionField descriptionTag="description"/>
-          
+          <InputDescriptionField descriptionTag="overview" />
+
           <InputDateField
             startDateTitle="開始日"
-            startDateTag="startTerm"
+            startDateTag="startDate"
             endDateTitle="終了日"
-            endDateTag="endTerm"/>
+            endDateTag="endDate"
+          />
 
-          <InputCoordinateField latitudeTag="latitude" longitudeTag="longitude"/>
+          <InputCoordinateField
+            latitudeTag="latitude"
+            longitudeTag="longitude"
+          />
 
-          <InputFileField title="バッジ画像" imageTag="badgeImage" nameTag="badgeName"/>
+          <InputFileField
+            title="バッジ画像"
+            imageTag="badgeImg"
+            nameTag="badgeName"
+          />
 
-          <InputFileField title="撮影対象画像" imageTag="picture" nameTag="pictureName"/>
+          <InputFileField
+            title="撮影対象画像"
+            imageTag="targetImg"
+            nameTag="targetName"
+          />
 
           <input type="submit" value="登録" />
         </form>
       </FormProvider>
     </PageTemplate>
-  )
+  );
 }
--- web/src/schemas/eventSchema.ts
+++ web/src/schemas/eventSchema.ts
@@ -1,45 +1,39 @@
 import { z } from "zod";
 
 export const eventSchema = z
-.object({
-    organizer: z.string().min(1, { message: "主催者を入力してください" }),
-    eventName: z.string().min(1, { message: "イベント名を入力してください" }),
-    description: z.string().min(1, { message: "イベントの説明を入力してください" }),
-    startTerm: z
-    .date({ message: "開始日を入力してください" }),
-    endTerm: z.date({ message: "終了日を入力してください" }),
-    latitude: z
-    .coerce
-    .number()
-    .min(-90, {message: "範囲は-90~90です"})
-    .max(90, {message: "範囲は-90~90です"}),
-    longitude: z
-    .coerce
-    .number()
-    .min(-180, {message: "範囲は-180~180です"})
-    .max(180, {message: "範囲は-180~180です"}),
+  .object({
+    name: z.string().min(1, { message: "イベント名を入力してください" }),
+    overview: z
+      .string()
+      .min(1, { message: "イベントの説明を入力してください" }),
+    startDate: z.date({ message: "開始日を入力してください" }),
+    endDate: z.date({ message: "終了日を入力してください" }),
+    latitude: z.coerce
+      .number()
+      .min(-90, { message: "範囲は-90~90です" })
+      .max(90, { message: "範囲は-90~90です" }),
+    longitude: z.coerce
+      .number()
+      .min(-180, { message: "範囲は-180~180です" })
+      .max(180, { message: "範囲は-180~180です" }),
     badgeName: z
-    .string()
-    .min(1, {message: "バッジの名前を入力してください"})
-    .regex(/^[A-Za-z0-9]+$/, { message: "入力は半角英数字のみです", }),
-    badgeImage: z
-    .custom<FileList>()
-    .refine(( data ) => data.length == 1, {
-        message: "画像ファイルが選択されていません" , 
+      .string()
+      .min(1, { message: "バッジの名前を入力してください" })
+      .regex(/^[A-Za-z0-9]+$/, { message: "入力は半角英数字のみです" }),
+    badgeImg: z.custom<FileList>().refine((data) => data.length == 1, {
+      message: "画像ファイルが選択されていません",
     }),
-    pictureName: z
-    .string()
-    .min(1, {message: "写真の名前を入力してください"})
-    .regex(/^[A-Za-z0-9]+$/, { message: "入力は半角英数字のみです", }),
-    picture: z
-    .custom<FileList>()
-    .refine(( data ) => data.length == 1, {
-        message: "画像ファイルが選択されていません" , 
+    targetName: z
+      .string()
+      .min(1, { message: "写真の名前を入力してください" })
+      .regex(/^[A-Za-z0-9]+$/, { message: "入力は半角英数字のみです" }),
+    targetImg: z.custom<FileList>().refine((data) => data.length == 1, {
+      message: "画像ファイルが選択されていません",
     }),
-})
-.refine(( data ) => data.startTerm <= data.endTerm, { 
-    path: [ "startTerm" ], 
-    message: "開始日は終了日より前である必要があります" , 
-});
+  })
+  .refine((data) => data.startDate <= data.endDate, {
+    path: ["startTerm"],
+    message: "開始日は終了日より前である必要があります",
+  });
 
 export type eventSchemaType = z.infer<typeof eventSchema>;
