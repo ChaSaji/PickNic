508e97f758415e5f53c57a78f045dc166c9940a0
Ilekaede <asakuramotiyo@gmail.com>
2024-07-20 23:50:18 +0900

    fix: R2の画像パスを{event_id}/user-photos/{user_name} にした

--- api/api/cruds/photo2user.py
+++ api/api/cruds/photo2user.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from api.models.database_models import Photo,Photo2MobileUser, MobileUser
 from api.schemes.photo2user import Photo2UserCreate, Photo2UserUpdate
 from api.lib.upload_image_to_s3 import upload_image_to_s3
-from api.cruds.mobile import get_event_photo_by_id
+from api.cruds.mobile import get_event_photo_by_id, get_mobile_user_by_Id
 import os
 
 def get_photo2Mobile_Relation_by_id(db: Session, id: int):
@@ -48,11 +48,17 @@ def update_photo2Mobile_Relation_by_id(db: Session, UpdateItem:Photo2UserUpdate)
 
 
 def update_user_photo(db:Session, contents:bytes, user_id:str, event_id:int):
+
+    mobile_user= get_mobile_user_by_Id(db, user_id)
+    if mobile_user is None:
+        raise ValueError(f"No user found for user_id {user_id} in update_user_photo()")
+    user_name = mobile_user.name
+
     aws_access_key_id = os.getenv('AWS_ACCESS_KEY')
     aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
     endpoint_url=os.getenv('R2_ENDPOINT_URL')
     bucket_name = os.getenv('S3_BUCKET_NAME')
-    file_key = upload_image_to_s3(aws_access_key_id, aws_secret_access_key, endpoint_url, bucket_name, contents, user_id, event_id)
+    file_key = upload_image_to_s3(aws_access_key_id, aws_secret_access_key, endpoint_url, bucket_name, contents, user_name, event_id)
 
     photo = get_event_photo_by_id(db, event_id)
     if photo is None:
--- api/api/lib/upload_image_to_s3.py
+++ api/api/lib/upload_image_to_s3.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def upload_image_to_s3(aws_access_key_id, aws_secret_access_key, endpoint_url, bucket_name, contents, user_id, event_id):
+def upload_image_to_s3(aws_access_key_id, aws_secret_access_key, endpoint_url, bucket_name, contents, user_name, event_id):
     # S3クライアントの作成
     s3 = boto3.client('s3',
                       region_name='auto',
@@ -12,9 +12,9 @@ def upload_image_to_s3(aws_access_key_id, aws_secret_access_key, endpoint_url, b
                       aws_access_key_id=aws_access_key_id,
                       aws_secret_access_key=aws_secret_access_key)
 
-    file_key = f"{event_id}/user-photos/{user_id}.jpg" # {event_id}/user-photos/{user_id}.png．
+    file_key = f"{event_id}/user-photos/{user_name}.jpg" # {event_id}/user-photos/{user_name}.jpg
     # 画像をアップロード
     s3.put_object(Bucket=bucket_name, Key=file_key, Body=contents)
-    print(f"Image successfully updated to S3: {user_id}")
+    print(f"Image successfully updated to S3: {user_name}")
 
     return file_key
