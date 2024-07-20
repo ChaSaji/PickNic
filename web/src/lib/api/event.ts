a1c62eee189105bdb062d32413583d7a57064223
Gata425 <107904801+Gata425@users.noreply.github.com>
2024-07-20 23:56:26 +0900

    encode関数作成

--- web/src/lib/api/event.ts
+++ web/src/lib/api/event.ts
@@ -3,6 +3,7 @@ import { fetchAPIWithAuth } from "./helper";
 import { Event, EventDetail } from "@/types/event";
 import { ApiError } from "./ApiError";
 import { eventSchemaType, eventPutSchemaType } from "@/schemas/eventSchema";
+import { encodeImage } from "@/lib/utils/encodeImage"
 
 export const getEvent = async ({
   eventId,
@@ -67,6 +68,23 @@ export const getEventList = async (): Promise<ApiResponse<Event[]>> => {
 export const postEventForm = async (
   event: eventSchemaType
 ): Promise<ApiResponse<Event>> => {
+  const fileList = event.targetImg
+
+  // バリデーション: ファイルが1つ選択されているかどうかを確認
+  if (!fileList || fileList.length !== 1) {
+    console.error('画像ファイルが選択されていません');
+  }
+
+  const dataURL = await encodeImage(fileList[0])
+
+  await fetch(`/api/r2?key=123/target.jpg`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({key: '123/target.jpg', body: dataURL}),
+  });
+
   try {
     const response = await fetchAPIWithAuth({
       endpoint: "events/create",
