c1417f5b2f2d545efa5d0ff1863d024dbd3123b3
bondroid1322 <bondroid3.22@icloud.com>
2024-07-21 20:52:56 +0900

    add: 投稿された写真一覧にLoadingコンポーネントを実装したよ

--- web/src/app/(app)/events/[eventId]/pictures/page.tsx
+++ web/src/app/(app)/events/[eventId]/pictures/page.tsx
@@ -6,11 +6,13 @@ import { useParams, useRouter } from "next/navigation";
 import { getPhotosFromR2 } from "@/lib/api/event";
 import { toast } from "react-toastify";
 import Button from "@/components/Button/Button";
+import Loading from "@/components/Loading/Loading";
 
 const EventPicturePage = () => {
   const params = useParams();
   const eventId = params["eventId"] as string;
   const [photos, setPhotos] = useState<Array<{ src: string; alt: string }>>([]);
+  const [loading, setLoading] = useState(true);
   const route = useRouter();
 
   useEffect(() => {
@@ -23,6 +25,8 @@ const EventPicturePage = () => {
         }
       } catch (error) {
         toast.error("Error fetching data:");
+      } finally {
+        setLoading(false);
       }
     })();
   }, []);
@@ -33,63 +37,84 @@ const EventPicturePage = () => {
   };
 
   return (
-    <PageTemplate titleLabel="投稿された写真">
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <Button onClick={() => route.push(`/events/${eventId}`)} label="戻る" />
-      </div>
-      <div
-        id="displayphoto"
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(4, 1fr)",
-          justifyContent: "center",
-          gap: "20px",
-          width: "100%",
-          marginTop: 20,
-          marginBottom: 20,
-        }}
-      >
-        {photos.map((photo, index) => (
+    <PageTemplate
+      titleLabel="投稿された写真"
+      style={loading ? { marginBottom: 200 } : { marginBottom: 0 }}
+    >
+      {loading ? (
+        <div
+          style={{
+            display: "flex",
+            height: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Loading />
+        </div>
+      ) : (
+        <>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <Button
+              onClick={() => route.push(`/events/${eventId}`)}
+              label="戻る"
+            />
+          </div>
           <div
+            id="displayphoto"
             style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
+              display: "grid",
+              gridTemplateColumns: "repeat(4, 1fr)",
+              justifyContent: "center",
+              gap: "20px",
               width: "100%",
-              height: "100%",
+              marginTop: 20,
+              marginBottom: 20,
             }}
-            key={index}
           >
-            {photo.alt && (
+            {photos.map((photo, index) => (
               <div
                 style={{
-                  marginBottom: 10,
-                  textAlign: "center",
-                  maxWidth: "100%",
-                  fontWeight: "bold",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  width: "100%",
+                  height: "100%",
                 }}
+                key={index}
               >
-                {getFileName(photo.alt)}
+                {photo.alt && (
+                  <div
+                    style={{
+                      marginBottom: 10,
+                      textAlign: "center",
+                      maxWidth: "100%",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {getFileName(photo.alt)}
+                  </div>
+                )}
+                <div
+                  style={{
+                    position: "relative",
+                    width: "100%",
+                    height: "auto",
+                  }}
+                >
+                  {photo.src && (
+                    <img
+                      src={photo.src}
+                      alt={getFileName(photo.alt)}
+                      style={{ width: "100%", height: "auto" }}
+                    />
+                  )}
+                </div>
               </div>
-            )}
-            <div
-              style={{
-                position: "relative",
-                width: "100%",
-                height: "auto",
-              }}
-            >
-              {photo.src && (
-                <img
-                  src={photo.src}
-                  alt={getFileName(photo.alt)}
-                  style={{ width: "100%", height: "auto" }}
-                />
-              )}
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
     </PageTemplate>
   );
 };
